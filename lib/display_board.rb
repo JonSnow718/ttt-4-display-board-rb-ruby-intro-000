71b6ac25874ed029e3973543481e1500ba072fe1
Jason Calvo <jason.calvo7@gmail.com>
2020-03-31 09:20:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,57 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]}| #{board[1]}  | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]}| #{board[4]}  | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]}| #{board[7]}  | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+def move(board,index,player)
+  board[index] = player 
+end
+
+
+def position_taken?(board,index)
+  if (board[index] == " ") || (board[index] == " ") || (board[index] == nill)
+    return false 
+  else 
+    return true
+  end
+
+  
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  end
+end
+  
+def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces = "X" || spaces = "O"
+      counter += 1 
+    end
+  end counter
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  end
+end
+
+
+
+    
+    
+
+  
+  
+  
