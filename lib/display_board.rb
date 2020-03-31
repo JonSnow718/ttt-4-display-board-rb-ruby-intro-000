7798c1f0ad442f2354fdb1f01b7a274dcd752b46
Jason Calvo <jason.calvo7@gmail.com>
2020-03-31 23:47:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -20,27 +20,3 @@ def move(board,index,player)
 end
 
 
-def position_taken?(board,index)
-  if (board[index] == " ") || (board[index] == " ") || (board[index] == nill)
-    return false 
-  else 
-    return true
-  end
-
-  
-def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    return true
-  end
-  
-def turn_count(board)
-  counter = 0
-  board.each do |spaces|
-    if spaces = "X" || spaces = "O"
-      counter += 1 
-    end
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
