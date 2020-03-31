8d053abcfa1e4c0978ffacc47e78d0e4d852f03d
Jason Calvo <jason.calvo7@gmail.com>
2020-03-31 23:51:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,12 +11,12 @@ def display_board(board)
 end
 
 ##def input_to_index(user_input)
-  user_input.to_i - 1
+  ##user_input.to_i - 1
 ##end
 
 
-def move(board,index,player)
-  board[index] = player 
-end
+##def move(board,index,player)
+  ##board[index] = player 
+##end
 
 
